9561ea3abe7ca1ac1a875a542d2c51724a27e2c0
cehann <zhangshihan@zhangshihandeMacBook-Air.local>
2021-06-19 20:29:38 +0800

    update K_anonymity

--- server/K_anonymity.py
+++ server/K_anonymity.py
@@ -3,10 +3,13 @@ from gensim.models import Word2Vec
 import random
 
 model = Word2Vec.load('./word2vec.model')
-def replace(msg, k=20):
-  msg_list = list(jieba.cut(msg, cut_all=False))
-  #print("Default Mode: " + "/ ".join(msg_list))
-  ans = []
-  for word in (msg_list): 
-    ans.append(model.wv.most_similar(word, topn=k)[random.randint(0, k-1)][0])
-  return "".join(ans)
+def replace(msg, k):
+    msg_list = list(jieba.cut(msg, cut_all=False))
+    ans = [] 
+    for word in (msg_list):
+        if word in model.wv.key_to_index:
+            ans.append(model.wv.most_similar(word, topn=k)[random.randint(0, k-1)][0])
+        else:
+            ans.append(word)
+    return "".join(ans)
+
